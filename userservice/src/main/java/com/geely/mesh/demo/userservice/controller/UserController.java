a0dfe9a3d2240ed51492f79833e31c608f14dc2a
unknown <Qijia.Wang@geely.com>
2018-09-20 18:16:16 +0800

    add prometheus monitor

--- userservice/src/main/java/com/geely/mesh/demo/userservice/controller/UserController.java
+++ userservice/src/main/java/com/geely/mesh/demo/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.geely.mesh.demo.userservice.controller;
 
 import com.geely.mesh.demo.userservice.domain.User;
+import com.geely.mesh.demo.userservice.monitor.PrometheusMetrics;
 import com.geely.mesh.demo.userservice.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -16,27 +17,41 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PrometheusMetrics
     @ApiOperation(value="获取用户列表", notes="")
     @RequestMapping(value="/", method=RequestMethod.GET)
     public List<User> getUserList() {
         return userService.getAllUsers();
     }
 
+    @PrometheusMetrics
     @ApiOperation(value="创建用户", notes="根据User对象创建用户")
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value="/", method=RequestMethod.POST)
     public String postUser(@RequestBody User user) {
         Long userId = userService.saveUser(user);
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "success: [userid:" + userId + "]";
     }
 
+    @PrometheusMetrics
     @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
     @ApiImplicitParam(name = "userid", value = "用户ID", required = true, dataType = "Long", paramType = "path")
     @RequestMapping(value="/{userid}", method=RequestMethod.GET)
     public User getUser(@PathVariable Long userid) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return userService.getUserById(userid);
     }
 
+    @PrometheusMetrics
     @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userid", value = "用户ID", required = true, dataType = "Long", paramType = "path"),
@@ -49,6 +64,7 @@ public class UserController {
         return "success";
     }
 
+    @PrometheusMetrics
     @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
     @ApiImplicitParam(name = "userid", value = "用户ID", required = true, dataType = "Long", paramType = "path")
     @RequestMapping(value="/{userid}", method=RequestMethod.DELETE)
