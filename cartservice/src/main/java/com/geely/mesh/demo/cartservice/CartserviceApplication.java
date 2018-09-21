c09c16275755a6e1d8c7176d7a6e059b123573df
unknown <Qijia.Wang@geely.com>
2018-09-21 17:08:23 +0800

    init cartservice orderservice inventoryservice

---
+++ cartservice/src/main/java/com/geely/mesh/demo/cartservice/CartserviceApplication.java
@@ -0,0 +1,12 @@
+package com.geely.mesh.demo.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartserviceApplication.class, args);
+	}
+}
